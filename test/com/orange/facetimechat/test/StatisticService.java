945d832dd6c94506f9f11a49f7aac38ff3c56f95
larmbr <nasa4836@gmail.com>
2012-07-27 15:28:29 +0800

    modify StaticSerice.java in test

--- test/com/orange/facetimechat/test/StatisticService.java
+++ test/com/orange/facetimechat/test/StatisticService.java
@@ -4,7 +4,9 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import org.apache.cassandra.cli.CliParser.newColumnFamily_return;
 import org.apache.log4j.Logger;
+import org.apache.thrift.transport.TFileTransport.chunkState;
 
 import com.orange.facetimechat.model.FacetimeUser;
 import com.orange.facetimechat.model.FacetimeUserManager;
@@ -13,8 +15,8 @@ import com.orange.game.model.manager.UserManager;
 public class StatisticService {
 
 	private static final Logger logger = Logger.getLogger(StatisticService.class.getName());
-	private AtomicInteger doubleMatchCount = new AtomicInteger(0);
-	private FacetimeUserManager userManager = FacetimeUserManager.getInstance();
+//	private AtomicInteger doubleMatchCount = new AtomicInteger(0);
+//	private FacetimeUserManager userManager = FacetimeUserManager.getInstance();
 	
 	// thread-safe singleton implementation
 	private static StatisticService defaultService = new StatisticService();
@@ -28,25 +30,48 @@ public class StatisticService {
 	
 	CopyOnWriteArrayList<String> facetimeUserList = new CopyOnWriteArrayList<String>();
 	ConcurrentHashMap<String, String> facetimeMatchUserList1 = new ConcurrentHashMap<String, String>();
-	ConcurrentHashMap<String, String> facetimeMatchUserList2 = new ConcurrentHashMap<String, String>();
+//	ConcurrentHashMap<String, String> facetimeMatchUserList2 = new ConcurrentHashMap<String, String>();
 	
 	public void addNewFacetime(String userId){
 		facetimeUserList.add(userId);
-		logger.info("[STATISTIC] total add user count="+facetimeUserList.size());
+		logger.info("<STATISTIC> total add user count="+facetimeUserList.size());
 	}
 	
 	public void addFacetimeMatch(String userId, String matchedUserId){
-		if (facetimeMatchUserList1.containsKey(userId)){
-			// already has user 
-			int count = doubleMatchCount.incrementAndGet();
-			logger.info("double match count="+count);
-			
-			logger.warn("[STATISTIC] user id has been added... userId="+userId+
-					", matchUserId="+facetimeMatchUserList1.get(userId)+", try to add "+matchedUserId);
-			return;
+		try {
+			if ( isMatchPairAdded(userId, matchedUserId) == true ) {
+				return;
+			}
+			facetimeMatchUserList1.put(userId, matchedUserId);
+			logger.info("<STATISTIC> match user map count="+ facetimeMatchUserList1.size() 
+					+ ", map="+facetimeMatchUserList1.toString());
+		} catch (Exception e) {
+			logger.error("<STATISTIC> duplicated match error!!!");
 		}
-		facetimeMatchUserList1.put(userId, matchedUserId);
-		logger.info("[STATISTIC] match user map count="+ facetimeMatchUserList1.size() + ", map="+facetimeMatchUserList1.toString());
-//		facetimeMatchUserList2.put(matchedUserId, userId);		
+	}
+	
+	private boolean isMatchPairAdded(String userId, String matchedUserId) throws Exception {
+		if (facetimeMatchUserList1.containsKey(userId) ) {
+			
+			if ( !facetimeMatchUserList1.get(userId).equals(matchedUserId)) {
+				logger.error("<STATISTIC> A !!! " + userId + " matched another one :"+
+						facetimeMatchUserList1.get(userId) + "!!!");
+				throw new Exception("duplicate match!!!:"+ userId);
+			} else 
+				return true;
+		
+		} 
+		else if (facetimeMatchUserList1.containsValue(userId))
+		{
+			
+			if ( !facetimeMatchUserList1.get(matchedUserId).equals(userId)) {
+				logger.error("<STATISTIC> P !!! " + matchedUserId + " matched another one:"+ 
+						facetimeMatchUserList1.get(matchedUserId)+ "!!!");
+				throw new Exception("duplicate match!!!:"+ matchedUserId);
+			} else 
+				return true;
+		
+		}
+		return false;
 	}
 }
